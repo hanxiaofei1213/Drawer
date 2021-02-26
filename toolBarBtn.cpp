81d4ce9090dae68440077bb2f7ae8edba24ebbeb
文喜 王 <wangwenxi@cmcm.com>
2021-02-26 17:46:38 +0800

    修改

--- toolBarBtn.cpp
+++ toolBarBtn.cpp
@@ -15,8 +15,8 @@ ToolbarBtn::~ToolbarBtn() {
 
 /**
  * @brief 设置工具栏按钮文本
- * @param a_text 名字
- * @param a_toolbarHwnd 包含此工具栏按钮的窗口句柄
+ * @param text 名字
+ * @param toolbarHwnd 包含此工具栏按钮的窗口句柄
  */
 void ToolbarBtn::setText(LPCTSTR text) {
 	TBBUTTONINFO info = { sizeof(TBBUTTONINFO), TBIF_TEXT, 0, 0, 0, 0, 0, 0, (LPTSTR)text, sizeof(text) };
